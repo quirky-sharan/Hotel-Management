e533bd5e4feca1574b85ee58555a78accd6833c2
quirky-sharan <imsharansoni@gmail.com>
2026-01-18 17:25:27 +0530

    Landing Page Designing

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,16 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: "class",
-  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "gold-400": "#b38b00",
+        "gold-500": "#d4af37",
+      },
+    },
   },
   plugins: [],
 };
